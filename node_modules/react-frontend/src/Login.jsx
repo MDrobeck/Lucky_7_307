528683a0d47b514d517eb544ffe25b06400ff70c
timchen0323 <tchen126@calpoly.edu>
2025-02-20 18:03:56 -0800

    ran prettier

--- node_modules/react-frontend/src/Login.jsx
+++ node_modules/react-frontend/src/Login.jsx
@@ -1,63 +1,64 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Login(props) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [creds, setCreds] = useState({
-    username: "",
-    pwd: ""
-  });
+	const [creds, setCreds] = useState({
+		username: "",
+		pwd: ""
+	});
 
-  return (
-    <form>
-      <label htmlFor="username">UserName</label>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        value={creds.username}
-        onChange={handleChange}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        value={creds.pwd}
-        onChange={handleChange}
-      />
-      <input
-        type="button"
-        value={props.buttonLabel || "Log In"}
-        onClick={submitForm}
-      />
-    </form>
-  );
+	return (
+		<form>
+			<label htmlFor="username">UserName</label>
+			<input
+				type="text"
+				name="username"
+				id="username"
+				value={creds.username}
+				onChange={handleChange}
+			/>
+			<label htmlFor="password">Password</label>
+			<input
+				type="password"
+				name="password"
+				id="password"
+				value={creds.pwd}
+				onChange={handleChange}
+			/>
+			<input
+				type="button"
+				value={props.buttonLabel || "Log In"}
+				onClick={submitForm}
+			/>
+		</form>
+	);
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    switch (name) {
-      case "username":
-        setCreds({ ...creds, username: value });
-        break;
-      case "password":
-        setCreds({ ...creds, pwd: value });
-        break;
-    }
-  }
+	function handleChange(event) {
+		const { name, value } = event.target;
+		switch (name) {
+			case "username":
+				setCreds({ ...creds, username: value });
+				break;
+			case "password":
+				setCreds({ ...creds, pwd: value });
+				break;
+		}
+	}
 
-  function submitForm() {
-    props.handleSubmit(creds)
-      .then(() => {
-        navigate("/"); // Navigate to home after successful login/signup
-      })
-      .catch((error) => {
-        // Error handling can be done here if needed
-        console.error("Login/Signup failed:", error);
-      });
-    setCreds({ username: "", pwd: "" });
-  }
+	function submitForm() {
+		props
+			.handleSubmit(creds)
+			.then(() => {
+				navigate("/"); // Navigate to home after successful login/signup
+			})
+			.catch((error) => {
+				// Error handling can be done here if needed
+				console.error("Login/Signup failed:", error);
+			});
+		setCreds({ username: "", pwd: "" });
+	}
 }
 
 export default Login;
--- node_modules/react-frontend/src/MyApp.jsx
+++ node_modules/react-frontend/src/MyApp.jsx
@@ -6,176 +6,188 @@ import Login from "./Login.jsx";
 import ToDoPage from "./ToDoPage";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
-  const INVALID_TOKEN = "INVALID_TOKEN";
-  const [token, setToken] = useState(INVALID_TOKEN);
-  const [message, setMessage] = useState("");
+	const [characters, setCharacters] = useState([]);
+	const INVALID_TOKEN = "INVALID_TOKEN";
+	const [token, setToken] = useState(INVALID_TOKEN);
+	const [message, setMessage] = useState("");
 
-  function removeOneCharacter(index) {
-    const person = characters[index]
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    deleteUser(person)
-      .then((res) => {if (res.status === 204) setCharacters(updated)})
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+	function removeOneCharacter(index) {
+		const person = characters[index];
+		const updated = characters.filter((character, i) => {
+			return i !== index;
+		});
+		deleteUser(person)
+			.then((res) => {
+				if (res.status === 204) setCharacters(updated);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  function updateList(person) {
-    postUser(person)
-      .then((res) => {if (res.status === 201) return res.json()})
-      .then((json) => {setCharacters([...characters, json])})
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+	function updateList(person) {
+		postUser(person)
+			.then((res) => {
+				if (res.status === 201) return res.json();
+			})
+			.then((json) => {
+				setCharacters([...characters, json]);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  function fetchUsers() {
-    console.log("fetchUsers is called. Current token:", token);
-    const promise = fetch(`http://localhost:8000/users`, {
-      headers: addAuthHeader()
-    });
-  
-    return promise;
-  }
+	function fetchUsers() {
+		console.log("fetchUsers is called. Current token:", token);
+		const promise = fetch(`http://localhost:8000/users`, {
+			headers: addAuthHeader()
+		});
 
-  useEffect(() => {
-    console.log("useEffect is running. Current token:", token);
-    fetchUsers()
-    .then((res) =>
-      res.status === 200 ? res.json() : undefined
-    )
-    .then((json) => {
-      if (json) {
-        setCharacters(json["users_list"]);
-      } else {
-        setCharacters(null);
-      }
-    })
-  }, [token]);
+		return promise;
+	}
 
-  function postUser(person) {
-    const promise = fetch("Http://localhost:8000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(person)
-    });
-  
-    return promise;
-  }
+	useEffect(() => {
+		console.log("useEffect is running. Current token:", token);
+		fetchUsers()
+			.then((res) =>
+				res.status === 200 ? res.json() : undefined
+			)
+			.then((json) => {
+				if (json) {
+					setCharacters(json["users_list"]);
+				} else {
+					setCharacters(null);
+				}
+			});
+	}, [token]);
 
-  function deleteUser(person) {
-    const promise = fetch("Http://localhost:8000/users/" + person.id, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(person)
-    });
-  
-    return promise;
-  }
+	function postUser(person) {
+		const promise = fetch("Http://localhost:8000/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(person)
+		});
 
-  // auth stuff
-  function loginUser(creds) {
-    const promise = fetch(`http://localhost:8000/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(creds)
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          response
-            .json()
-            .then((payload) => setToken(payload.token));
-          setMessage(`Login successful; auth token saved`);
-          console.log("Loggedin. Current token:", token);
-        } else {
-          setMessage(
-            `Login Error ${response.status}: ${response.data}`
-          );
-        }
-      })
-      .catch((error) => {
-        setMessage(`Login Error: ${error}`);
-      });
-  
-    return promise;
-  }
+		return promise;
+	}
 
-  function signupUser(creds) {
-    const promise = fetch(`http://localhost:8000/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(creds)
-    })
-      .then((response) => {
-        if (response.status === 201) {
-          response
-            .json()
-            .then((payload) => setToken(payload.token));
-          setMessage(
-            `Signup successful for user: ${creds.username}; auth token saved`
-          );
-        } else {
-          setMessage(
-            `Signup Error ${response.status}: ${response.data}`
-          );
-        }
-      })
-      .catch((error) => {
-        setMessage(`Signup Error: ${error}`);
-      });
-  
-    return promise;
-  }
+	function deleteUser(person) {
+		const promise = fetch(
+			"Http://localhost:8000/users/" + person.id,
+			{
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(person)
+			}
+		);
 
-  function addAuthHeader(otherHeaders = {}) {
-    console.log("addAuthHeader is called. Current token:", token);
-    if (token === INVALID_TOKEN) {
-      return otherHeaders;
-    } else {
-      return {
-        ...otherHeaders,
-        Authorization: `Bearer ${token}`
-      };
-    }
-  }
+		return promise;
+	}
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Login Route */}
-        <Route
-          path="/login"
-          element={<Login handleSubmit={loginUser} message={message} />}
-        />
+	// auth stuff
+	function loginUser(creds) {
+		const promise = fetch(`http://localhost:8000/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(creds)
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					response
+						.json()
+						.then((payload) => setToken(payload.token));
+					setMessage(`Login successful; auth token saved`);
+					console.log("Loggedin. Current token:", token);
+				} else {
+					setMessage(
+						`Login Error ${response.status}: ${response.data}`
+					);
+				}
+			})
+			.catch((error) => {
+				setMessage(`Login Error: ${error}`);
+			});
 
-        {/* Main Content Route */}
-        <Route
-          path="/"
-          element={
-            <ToDoPage />
-          }
-        />
+		return promise;
+	}
 
-        <Route
-          path="/signup"
-          element={
-            <Login handleSubmit={signupUser} buttonLabel="Sign Up" />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+	function signupUser(creds) {
+		const promise = fetch(`http://localhost:8000/signup`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(creds)
+		})
+			.then((response) => {
+				if (response.status === 201) {
+					response
+						.json()
+						.then((payload) => setToken(payload.token));
+					setMessage(
+						`Signup successful for user: ${creds.username}; auth token saved`
+					);
+				} else {
+					setMessage(
+						`Signup Error ${response.status}: ${response.data}`
+					);
+				}
+			})
+			.catch((error) => {
+				setMessage(`Signup Error: ${error}`);
+			});
+
+		return promise;
+	}
+
+	function addAuthHeader(otherHeaders = {}) {
+		console.log("addAuthHeader is called. Current token:", token);
+		if (token === INVALID_TOKEN) {
+			return otherHeaders;
+		} else {
+			return {
+				...otherHeaders,
+				Authorization: `Bearer ${token}`
+			};
+		}
+	}
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* Login Route */}
+				<Route
+					path="/login"
+					element={
+						<Login
+							handleSubmit={loginUser}
+							message={message}
+						/>
+					}
+				/>
+
+				{/* Main Content Route */}
+				<Route path="/" element={<ToDoPage />} />
+
+				<Route
+					path="/signup"
+					element={
+						<Login
+							handleSubmit={signupUser}
+							buttonLabel="Sign Up"
+						/>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 
 	// return <ToDoPage />;
 }
